b316325e4952154cad0e882fd88b1f78ae864557
Alastor7676 <rahmanasifur99@gmail.com>
2023-03-22 18:54:32 +0600

    commit 5

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/testdev.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/testdev.java
@@ -4,4 +4,12 @@ public class testdev {
 
     private String course;
     private int course_no;
+
+    public int getCourse_no() {
+        return course_no;
+    }
+
+    public void setCourse_no(int course_no) {
+        this.course_no = course_no;
+    }
 }
