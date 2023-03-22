255d3d65208bbc9cc6f09cda54ebf06a51923431
Alastor7676 <rahmanasifur99@gmail.com>
2023-03-22 18:48:20 +0600

    1st commit

---
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/test.java
@@ -0,0 +1,28 @@
+package com.github.barteksc.pdfviewer;
+
+public class test {
+
+    private String name;
+    private int roll;
+
+    public test(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
